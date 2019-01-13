35aad37413ed610d41b89c1cc682be68de61eb45
ZheyiWang <123348687@qq.com>
2019-01-13 15:30:44 +0800

    init
    init

---
+++ consumer/src/main/java/com/wzy/consumer/controller/KafkaConsumer.java
@@ -0,0 +1,17 @@
+package com.wzy.consumer.controller;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+/**
+ * 消费者
+ * 使用@KafkaListener注解,可以指定:主题,分区,消费组
+ */
+@Component
+public class KafkaConsumer {
+
+    @KafkaListener(topics = {"app_log"})
+    public void receive(String message){
+        System.out.println("app_log--消费消息:" + message);
+    }
+}
--- producer/src/test/java/com/wzy/producer/ProducerApplicationTests.java
+++ producer/src/test/java/com/wzy/producer/ProducerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.wzy.springboot_demo;
+package com.wzy.producer;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringbootDemoApplicationTests {
+public class ProducerApplicationTests {
 
     @Test
     public void contextLoads() {
