8f73f5f5c3a770b214bf5b9de9a5a1141ef65793
ZheyiWang <123348687@qq.com>
2019-01-13 10:40:23 +0800

    init
    init

--- src/main/java/com/wzy/SpringbootDemoApplication.java
+++ src/main/java/com/wzy/SpringbootDemoApplication.java
@@ -2,10 +2,12 @@ package com.wzy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /*IDE中调试用*/
 
 @SpringBootApplication
+@EnableScheduling  //允许支持schedule
 public class SpringbootDemoApplication {
 
     public static void main(String[] args) {
