53e1fe7e9dbcdb0d949cf3650fbb3088709e0ce6
ZheyiWang <123348687@qq.com>
2019-01-20 20:41:31 +0800

    init
    init

---
+++ src/main/java/com/wzy/controller/FileDownloadController.java
@@ -0,0 +1,54 @@
+package com.wzy.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+
+@Controller
+public class FileDownloadController {
+
+    @RequestMapping("download")
+    @ResponseBody
+    public String downLoad(HttpServletResponse response){
+        String filename="20190119174147.jpg";
+        String filePath = "D:/test" ;
+        File file = new File(filePath + "/" + filename);
+        if(file.exists()){ //判断文件父目录是否存在
+            response.setContentType("application/force-download");
+            response.setHeader("Content-Disposition", "attachment;fileName=" + filename);
+
+            byte[] buffer = new byte[1024];
+            FileInputStream fis = null; //文件输入流
+            BufferedInputStream bis = null;
+
+            OutputStream os = null; //输出流
+            try {
+                os = response.getOutputStream();
+                fis = new FileInputStream(file);
+                bis = new BufferedInputStream(fis);
+                int i = bis.read(buffer);
+                while(i != -1){
+                    os.write(buffer);
+                    i = bis.read(buffer);
+                }
+
+            } catch (Exception e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            System.out.println("----------file download" + filename);
+            try {
+                bis.close();
+                fis.close();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+        return "文件不存在";
+    }
+
+}
